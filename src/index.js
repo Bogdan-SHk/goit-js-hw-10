33f954668a2b28b8056a2f4e9bd023cbc85ecbf2
Bogdan-SHk <bogdan.shkolnik@gmail.com>
2023-03-06 22:06:05 +0200

    Доробив ДЗ

--- src/index.js
+++ src/index.js
@@ -1,83 +1,84 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
+import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
-const countryValue = 'name,capital,population,flags,languages';
-
 const inputEl = document.querySelector('#search-box');
 const countryListEl = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
+
 inputEl.addEventListener('input', debounce(onCountrySearch, DEBOUNCE_DELAY));
 
 function onCountrySearch(evt) {
-    const searchCountry = evt.target.value.trim();
-    
-    if (!searchCountry) {
-        countryListEl.innerHTML = '';
-    }
+  const searchCountry = evt.target.value.trim();
 
-  function fetchCountries() {
-    const url = `https://restcountries.com/v3.1/name/${searchCountry}?fields=${countryValue}`;
-    return fetch(url).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    });
+  if (!searchCountry) {
+    countryListEl.innerHTML = '';
+    countryInfo.innerHTML = '';
   }
 
-  fetchCountries()
-    .then(countries => renderCountryList(countries))
-    .catch(error =>
-      Notiflix.Notify.failure('Oops, there is no country with that name')
-  );
-    
+  fetchCountries(searchCountry)
+    .then(countries => renderCountryMarkup(countries))
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      countryInfo.classList.add('invisible');
+      countryListEl.classList.add('invisible');
+    });
 }
 
-function renderCountryList(countries) {
+// Функції для створення та рендеру розмітки
+
+function renderCountryMarkup(countries) {
   if (countries.length > 10) {
     Notiflix.Notify.info(
       'Too many matches found. Please enter a more specific name.'
     );
-    console.log(countries.length);
+    countryInfo.classList.add('invisible');
+    countryListEl.classList.add('invisible');
   } else if (countries.length > 1 && countries.length < 10) {
-    console.log(countries.length);
-    const markup = countries
-      .map(country => {
-        return `
-          <li>
+    renderCountryList(countries);
+    countryInfo.classList.add('invisible');
+    countryListEl.classList.remove('invisible');
+  } else if (countries.length === 1) {
+    renderCountryInfo(countries);
+    countryInfo.classList.remove('invisible');
+    countryListEl.classList.add('invisible');
+  }
+}
+
+function renderCountryList(countries) {
+  const markup = countries
+    .map(country => {
+      return `
+          <li class="country-item">
             <p>${country.name.official}</p>
-            <svg width="20" height="20">
-                <use href="${country.flags.svg}"></use>
-            </svg>
+            <img src="${country.flags.svg}" width="25" height="auto"/>
           </li>
       `;
-      })
-      .join('');
+    })
+    .join('');
 
-    console.log(markup);
-    countryListEl.innerHTML = markup;
-  } else if (countries.length === 1) {
-    console.log(countries.length);
-    const markup = countries
-      .map(country => {
-        return `
-            <li>
-              <p>${country.name.official}</p>
-              <svg width="20" height="20">
-                  <use href="${country.flags.svg}"></use>
-              </svg>
-              <p><span>Capital:</span> ${country.capital}</p>
-              <p><span>Population:</span> ${country.population}</p>
-              <p><span>Languages:</span> ${(country.languages = Object.values(
-                country.languages
-              ).join(', '))}</p>
-            </li>
+  countryListEl.innerHTML = markup;
+}
+
+function renderCountryInfo(countries) {
+  const markup = countries
+    .map(({ name, flags, capital, population, languages }) => {
+      return `
+              <div class="container">
+              <h2>${name.official}</h2>
+              <img src="${flags.svg}" width="30" height="auto"/>
+              </div>
+              <ul class="info-list">
+              <li class="info-item"><span class="info-item_decor">Capital:</span> ${capital}</li>
+              <li class="info-item"><span class="info-item_decor">Population:</span> ${population}</li>
+              <li class="info-item"><span class="info-item_decor">Languages:</span> ${(languages =
+                Object.values(languages).join(', '))}</li>
+              </ul>
         `;
-      })
-      .join('');
+    })
+    .join('');
 
-    countryListEl.innerHTML = markup;
-  }
+  countryInfo.innerHTML = markup;
 }
